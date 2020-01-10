1eb17abc96889209fd89d2e2210aaac138753172
Poretga99 <zukovec96@gmail.com>
2020-01-10 17:50:41 +0100

    Dokumentacija kode

--- bspline_genetic_final.py
+++ bspline_genetic_final.py
@@ -7,47 +7,37 @@ from scipy.ndimage import gaussian_filter
 from skimage import feature
 from skimage.measure import compare_ssim as ssim
 
+
 def filter_image(img, sigma):
+    '''
+    Funkcija za glajenje slike (ponavadi razlika ref. in plav. slike) z Gaussovim filtrom.
+    :param img: vhodna slika
+    :param sigma: standardna deviacija za Gaussov kernel
+    :return: zglajena slika, standardizirana na interval [0, 1]
+    '''
     img = np.asarray(img, dtype=np.float)
     out_img = gaussian_filter(img, sigma)
-    # standardiziramo na interval [0, 1]
+    # standardiziramo zglajeno sliko na interval [0, 1]
     out_img = (out_img - np.min(out_img))/(np.max(out_img) - np.min(out_img))
     return out_img
 
 
 def normalizeImage(iImage, type='whitening'):
+    '''
+        Funkcija za normalizacijo vhodne slike.
+        :param iImage: vhodna slika za normalizacijo
+        :param type: tip standardizacije
+        :return: standardizirana slika
+    '''
     if type=='whitening':
         oImage = (iImage - np.mean(iImage)) / np.std(iImage)
     elif type=='range':
         oImage = (iImage - np.min(iImage)) / (np.max(iImage) - np.min(iImage))
     return oImage
 
-
-def getChessBoardImage(iImageSize, iArraySize=10, dtype='uint8'):
-    dy = int(np.ceil(iImageSize[0] / iArraySize)) + 1
-    dx = int(np.ceil(iImageSize[1] / iArraySize)) + 1
-
-    A = [255 * np.ones(shape=(iArraySize, iArraySize)),
-         np.zeros(shape=(iArraySize, iArraySize))]
-    board = np.array(np.vstack([np.hstack([A[(i + j) % 2] \
-                                           for i in range(dx)]) \
-                                for j in range(dy)]), dtype=dtype)
-    return board[:iImageSize[0], :iImageSize[1]]
-
-
-def swirlControlPoints(iCPx, iCPy, a=2.0, b=100.0):
-    oCPx = np.array(iCPx)
-    oCPy = np.array(iCPy)
-    xc = np.mean(oCPx[1:-3,1:-3])
-    yc = np.mean(oCPy[1:-3,1:-3])
-    rx1 = oCPx[1:-3,1:-3] - xc
-    ry1 = oCPy[1:-3,1:-3] - yc
-    angle = a*np.exp(-(rx1*rx1+ry1*ry1)/(b*b))
-    oCPx[1:-3,1:-3] = np.cos(angle)*rx1 + np.sin(angle)*ry1 + xc
-    oCPy[1:-3,1:-3] = -np.sin(angle)*rx1 + np.cos(angle)*ry1 + xc
-    return oCPx, oCPy
-
-
+###############################################
+# Definicija funkcij za B-zlepke
+# Definicija B0, B1, B2, B3 baznih funkcij B-zlepkov
 def B0(u):
     return (1 - u) ** 3 / 6
 
@@ -66,8 +56,14 @@ def B3(u):
 
 B = [B0, B1, B2, B3]
 
+
 def getCubicBSpline2DGrid(iImageSize, iStep):
-    # YOUR CODE HERE
+    '''
+    Funkcija za ustvarjanje kontrolnih tock na sliki.
+    :param iImageSize: velikost slike
+    :param iStep: zacetna razdalja med kontrolnimi tockami na sliki
+    :return: matriki tock v x in y smeri
+    '''
     dy, dx = iImageSize
     if not isinstance(iStep, (tuple, list)):
         iStep = (iStep, iStep)
@@ -92,6 +88,14 @@ def getCubicBSpline2DDeformation(iImageSize, iCPx, iCPy, iStep):
 
 
 def deformImageBSpline2D(iImage, iCPx, iCPy, iStep):
+    '''
+    Funkcija za deformacijo vhodne slike preko kontrolnih tock.
+    :param iImage: vhodna slika
+    :param iCPx: matrika x kontrolnih tock
+    :param iCPy: matrika y kontrolnih tock
+    :param iStep: korak
+    :return: transformirana slika
+    '''
     dy, dx = iImage.shape
     oGx, oGy = getCubicBSpline2DDeformation(iImage.shape, iCPx, iCPy, iStep)
     gx, gy = np.meshgrid(np.arange(dx), np.arange(dy))
@@ -105,11 +109,10 @@ def deformImageBSpline2D(iImage, iCPx, iCPy, iStep):
 
 def generate_points(iCPx, iCPy):
     '''
-    Function generates inner field of image, crops top left and bottom two right rows/columns, returns array of
-    [n_of_pts, 2] where rows consists of [row_i, column_i]
-    :param iCPx:
-    :param iCPy:
-    :return:
+    Generacija matrike tock preko matrik kontrolnih tock, za lazje racunanje z ostalimi funkcijami.
+    :param iCPx: matrika x kontrolnih tock
+    :param iCPy: matrika y kontrolnih tock
+    :return: matrika tock dimenzije [stevilo tock, 2]
     '''
     src = np.dstack([iCPx[1:-2,1:-2].flat, iCPy[1:-2,1:-2].flat])[0]
     src = np.asarray(src, dtype=np.uint8)
@@ -117,6 +120,11 @@ def generate_points(iCPx, iCPy):
 
 
 def modify_filtered_vals(vals):
+    '''
+    Funkcija za lokalni vpliv tock z visoko mutacijsko vrednostjo na bliznjo 3x3 okolico.
+    :param vals: matrika mutacijskih vrednosti
+    :return: modificirana matrika mutacijskih vrednosti.
+    '''
     tmp = np.zeros((vals.shape[0], vals.shape[1]), dtype=np.float)
     for i in range(1, vals.shape[0]-1):
         for j in range(1, vals.shape[1]-1):
@@ -129,6 +137,13 @@ def modify_filtered_vals(vals):
 
 
 def get_filtered_vals(cromosomes, im_filtered, filter_pts):
+    '''
+    Funkcija, ki vrne vrednost kontrolnih tock posameznega kromosoma na normalizirani zglajeni razliki slik [0, 1].
+    :param cromosomes: matrika kromosomov
+    :param im_filtered: matrika zglajnenih slik
+    :param filter_pts: matrika zacetnih lokacij kontrolnih tock
+    :return: vrednosti kontrolnih tock na zglajeni razliki slik
+    '''
     tmp = np.zeros((cromosomes.shape[2], cromosomes.shape[3]), dtype=np.float)
     tmp_vals = im_filtered[[filter_pts[:, 0], filter_pts[:, 1]]].reshape((cromosomes.shape[2]-3, cromosomes.shape[3]-3), order='F')
     tmp_vals = (tmp_vals - tmp_vals.min())/(tmp_vals.max()-tmp_vals.min())*0.8+0.2
@@ -139,6 +154,13 @@ def get_filtered_vals(cromosomes, im_filtered, filter_pts):
 
 
 def mi(im1, im2, bins):
+    '''
+    Definicija mere podobnosti - medsebojna informacija.
+    :param im1: slika 1
+    :param im2: slika 2
+    :param bins: stevilo predalov
+    :return: podobnost (skalar)
+    '''
     hist_2d, e_edges, y_edges = np.histogram2d(im1.ravel(), im2.ravel(), bins=bins)
     pxy = hist_2d / float(np.sum(hist_2d))
     px = np.sum(pxy, axis=1)
@@ -147,13 +169,20 @@ def mi(im1, im2, bins):
     nzs = pxy > 0
     return np.sum(pxy[nzs] * np.log(pxy[nzs] / px_py[nzs]))
 
+
 ###########################################
 ### implementacija genetskega algoritma
 
 
 def generate_cromosomes(num, init_locx, init_locy, rng=2):
-    # number of cromosomes, initial location of points, random number range
-    # returns array [num_of_cromosomes, initial_loc.shape[0], initial_loc.shape[1]]
+    '''
+    Funkcija za zacetno generacijo kromosomov.
+    :param num: stevilo kromosomov
+    :param init_locx: zacetne lokacija kontrolnih tock x
+    :param init_locy: zacetne lokacije kontrolnih tock y
+    :param rng: abs vrednost maksimalne mutacije
+    :return: matrika kromosomov
+    '''
     tmp = np.random.random((2, num, init_locx.shape[0], init_locx.shape[1]))*2*rng - rng
     tmp[0, :, :, :] += init_locx
     tmp[1, :, :, :] += init_locy
@@ -161,7 +190,14 @@ def generate_cromosomes(num, init_locx, init_locy, rng=2):
 
 
 def select_mating(population, fit, num_parents, deformed):
-    # inicializacija prazne matrike staršev
+    '''
+    Funkcija za izbor kromosomov starsev glede na najboljse vrednosti fitnes funkcije
+    :param population:
+    :param fit:
+    :param num_parents:
+    :param deformed:
+    :return:
+    '''
     # indeksi najmanjsih elementov kritejrijske funkcije
     min_idxs = abs(fit).argsort(axis=0)[:num_parents]
     # vrnemo x najboljse primerke za starse
@@ -169,6 +205,14 @@ def select_mating(population, fit, num_parents, deformed):
 
 
 def fitness(source_img, floating_img, cromosomes, iStep):
+    '''
+    Funkcija za izracun fitnesa danih kromosomov.
+    :param source_img: referencna slika
+    :param floating_img: plavajoca slika
+    :param cromosomes: matrika kromosomov
+    :param iStep: korak
+    :return: fitnes, zglajene slike, deformirane slike
+    '''
     source_img = np.asarray(source_img, dtype=np.float)
     floating_img = np.asarray(floating_img, dtype=np.float)
     fit = np.zeros((cromosomes.shape[1], 1), dtype=np.float)
@@ -185,21 +229,39 @@ def fitness(source_img, floating_img, cromosomes, iStep):
 
 
 def mutate(dst_points, rng, fit_f, flag, filt_pts, fixed, filtered):
+    '''
+    Funkcija za mutiranje vhodne matrike kromosomov.
+    :param dst_points: matrika kromosomov
+    :param rng: abs. vrednost maksimalne mutacije
+    :param fit_f: fitnesi kromosomov
+    :param flag: nepotreben parameter, stara implementacija
+    :param filt_pts: zacetne kontrolne tocke
+    :param fixed: nepotreben parameter, stara implementacija
+    :param filtered: matrika zglajenih slik
+    :return: mutirana matrika kromosomov
+    '''
     #rng *= flag
+    # generacija matrike mutacijskih vrednosti
     mutation = np.random.random((2, dst_points.shape[1], dst_points.shape[2], dst_points.shape[3]))*2*rng - rng
-
+    # sprememba mutacijske vrednosti glede na vrednost solezne zacetne kontrolne tocke na zglajeni razliki slik
     for i in range(dst_points.shape[1]):
         mutation[0, i, :, :] = np.multiply(mutation[0, i, :, :], 2*get_filtered_vals(cromosomes, filtered[i], filt_pts))
         mutation[1, i, :, :] = np.multiply(mutation[0, i, :, :], 2*get_filtered_vals(cromosomes, filtered[i], filt_pts))
+        # stara implementacija, kjer je bila mutacijska vrednost neposredno odvisna od fitnesa danega kromosoma.
         #mutation[0, i, :, :] *= 1#fit_f[i]
         #mutation[1, i, :, :] *= 1#fit_f[i]
-
-
+    # generacija matrike lokacij mutacije, kjer ima vsaka tocka verjetnost mutacije 1/2
     mutation_locations = np.random.randint(0, 2, (2, dst_points.shape[1], dst_points.shape[2], dst_points.shape[3]))
     return dst_points + np.multiply(mutation, mutation_locations)
 
 
 def crossover(parents):
+    '''
+    Funkcija za operacijo krizanja kromosomov. Iz nabora kromosomov nakljucno izbere 2, nato jima izmenja gene,
+    kjer je lokacija genov za vsak par kromosomov nakljucna.
+    :param parents: kromosomi starsev
+    :return: krizani kromosomi starsev -> potomci
+    '''
     children = np.zeros((2, int(parents.shape[1]/2), parents.shape[2], parents.shape[3]), dtype=np.float)
     n_parents, genes_rows, genes_cols = parents.shape[1], parents.shape[2], parents.shape[3]
     parents_ids = np.arange(n_parents)
@@ -213,45 +275,22 @@ def crossover(parents):
     return children
 
 
-################################################
-
-
-
-
-def normalizeImage(iImage, type='whitening'):
-    if type=='whitening':
-        oImage = (iImage - np.mean(iImage)) / np.std(iImage)
-    elif type=='range':
-        oImage = (iImage - np.min(iImage)) / (np.max(iImage) - np.min(iImage))
-    return oImage
-
-
 # optimizacija
 ###########################################################
 # Nalozimo slike
 
 fixed = normalizeImage(np.array(im.open('dojka1.png')))
 moving = normalizeImage(np.array(im.open('dojka2.png')))
-
-
-##############################################################
 fixed = np.asarray(fixed, dtype=np.float)
 moving = np.asarray(moving, dtype=np.float)
-#fixed[fixed > 5.0] = 254.0
-#moving[moving > 5.0] = 254.0
+##############################################################
 
-#edges1 = feature.canny(fixed, sigma=3)
-#edges2 = feature.canny(moving, sigma=3)
-
-#fixed = np.asarray(edges1, dtype=np.float)
-#moving = np.asarray(edges2, dtype=np.float)
-
-
-
+# stevilo kontrolnih tock na po x in y dimenziji
 iStep = (((fixed.shape[0]-1)/5), ((fixed.shape[1]-1)/5))
-
+# generacija matrike kontrolnih tock
 oCPx, oCPy = getCubicBSpline2DGrid(fixed.shape, iStep)
 
+# graficni prikaz lege kontrolnih tock
 plt.figure()
 plt.imshow(moving, cmap='gray')
 plt.plot(oCPx, oCPy, marker='o', color='r', linewidth=1)
@@ -261,20 +300,25 @@ plt.xlim([-50,250])
 plt.ylim([250,-50])
 plt.show()
 
+# stevec iteracij
 count = 0
 flag = 1
 fmin = 10
 flag_count = 0
 f_hist = np.ones(1, dtype=np.float)
+# generacija kromosmov
 cromosomes = generate_cromosomes(40, oCPx, oCPy, rng=2)
+# izracun fitnesov
 fit, filtered, deformed = fitness(fixed, moving, cromosomes, iStep)
-filt_pts = generate_points(oCPx, oCPy) # points in rectangle for filtered image value return
+# preoblikovanje matrike kontrolnih tock za lazje racunanje
+filt_pts = generate_points(oCPx, oCPy)
+# inicializacija prazne matrike, kamor bomo shranjevali deformirane slike
 deformed = np.zeros((cromosomes.shape[1], fixed.shape[0], fixed.shape[1]), dtype=np.float)
-
 for i in range(cromosomes.shape[1]):
     deformed[i, :, :] = fixed
 
-
+############################################################
+# Inicializacija za live prikaz trenutnega stanja optimizacije
 ax = plt.subplot(2,3,1)
 ax.title.set_text('Referencna')
 ax.imshow(fixed, cmap='gray')
@@ -303,7 +347,8 @@ ax5.title.set_text('Zglajena')
 
 plt.gcf().canvas.draw()
 plt.pause(0.5)
-
+###########################################################
+# Zacetek optimizacije, genetske operacije
 while count < 2000: #and fmin > 1.5:
     selected_cromosomes, selected_fit, deformed, min_idxs = select_mating(cromosomes, fit, 30, deformed)
     offspring = crossover(selected_cromosomes)
@@ -327,11 +372,13 @@ while count < 2000: #and fmin > 1.5:
     def_opt = deformed[idx, :, :]
     filt_opt = filtered2[idx, :, :]
     print("Count: ", count, " fmin: ", fmin, " flag:", flag, " fhist std", f_hist[-10:].var())
+    # stara implementacija, kjer se je spreminjala maksimalna vrednost mutacije podobno kot pri afini preslikavi
     if (f_hist[-10:].max() - f_hist[-10:].min()) < 1**-3 and len(f_hist) > 50 and flag_count >= 10:
         flag *= 1.0
         flag_count = 0
     flag_count += 1
 
+    # Posodobitev okna s trenutnim stanjem optimizacije
     ax3.images[0].set_data(def_opt)
     ax4.clear()
     ax4.imshow(def_opt, cmap='gray')
@@ -346,11 +393,10 @@ while count < 2000: #and fmin > 1.5:
     plt.pause(0.5)
 
 
-
-
-
+# Transformacija slike glede na najbolj optimalne parametre
 cbImageDeformed = deformImageBSpline2D(moving, T_optx, T_opty, iStep)
 
+# Prikaz rezultatov
 plt.figure()
 plt.subplot(1,3,1)
 plt.imshow(fixed, cmap='gray')
